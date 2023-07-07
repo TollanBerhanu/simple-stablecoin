fece77bff8d353efa12a06aac6d7cf5395ff3d73
TollanBerhanu <tollanberhanu@gmail.com>
2023-07-07 08:52:16 +0300

    fix: updated MintBurn UI

--- code/off-chain/src/User/MintBurn/MintBurn.tsx
+++ code/off-chain/src/User/MintBurn/MintBurn.tsx
@@ -4,6 +4,9 @@ const MintBurn = (props: any) => {
 
     const name = props.info.name
     const icon = props.info.icon
+    const buttonStyle = name == 'Mint' ? 'w-1/4 px-5 py-3 text-gray-700 font-bold bg-gradient-to-br font-medium rounded-lg text-lg text-center from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800' :
+                                         'w-1/4 px-5 py-3 text-gray-700 font-bold bg-gradient-to-br font-medium rounded-lg text-lg text-center from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800'
+  {/* A less dramatic Mint button style: 'w-1/4 px-5 py-3 text-gray-700 font-bold bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-lg text-center' */}
 
     return(
         <section className="py-8 px-4 m-6">
@@ -19,15 +22,9 @@ const MintBurn = (props: any) => {
                     <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray"> Enter amount to <span className="lowercase">{ name }</span> ... </label>
                     <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-4/5 p-2.5 dark:bg-gray-300 dark:border-gray-600 dark:placeholder-gray-700 dark:text-white dark:focus:ring-blue-200 dark:focus:border-blue-200" placeholder="Amount" required />
                 </div>
-                <button type="submit" className="w-1/4 px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> { name } </button>
-
-                {/* <button type="button" className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-                    Mint
+                <button type="button" className ={buttonStyle} >
+                    { name }
                 </button>
-
-                <button type="button" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-                    Burn
-                </button> */}
             
             </form>
 
