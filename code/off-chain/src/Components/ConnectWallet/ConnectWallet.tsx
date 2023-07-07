190935cf6356afebf7cf7690d8b4c70b48577fc3
TollanBerhanu <tollanberhanu@gmail.com>
2023-07-07 08:52:36 +0300

    feat: added state table for admin

--- code/off-chain/src/Components/ConnectWallet/ConnectWallet.tsx
+++ code/off-chain/src/Components/ConnectWallet/ConnectWallet.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ConnectWallet = () => (
+const ConnectWallet = (props: {dbState: Boolean}) => (
     
     <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-20 mx-20">
         <div className="flex flex-col items-center pb-10">
@@ -13,6 +13,87 @@ const ConnectWallet = () => (
                 {/* <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Message</a> */}
             </div>
         </div>
+
+        { props.dbState ?
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">
+                            Product name
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Color
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Category
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Price
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Action
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            Apple MacBook Pro 17"
+                        </th>
+                        <td className="px-6 py-4">
+                            Silver
+                        </td>
+                        <td className="px-6 py-4">
+                            Laptop
+                        </td>
+                        <td className="px-6 py-4">
+                            $2999
+                        </td>
+                        <td className="px-6 py-4">
+                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
+                    </tr>
+                    <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            Microsoft Surface Pro
+                        </th>
+                        <td className="px-6 py-4">
+                            White
+                        </td>
+                        <td className="px-6 py-4">
+                            Laptop PC
+                        </td>
+                        <td className="px-6 py-4">
+                            $1999
+                        </td>
+                        <td className="px-6 py-4">
+                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
+                    </tr>
+                    <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            Magic Mouse 2
+                        </th>
+                        <td className="px-6 py-4">
+                            Black
+                        </td>
+                        <td className="px-6 py-4">
+                            Accessories
+                        </td>
+                        <td className="px-6 py-4">
+                            $99
+                        </td>
+                        <td className="px-6 py-4">
+                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        : null }
+
+
     </div>
 
 )
