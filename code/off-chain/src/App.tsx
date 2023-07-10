f57c98affa1713d4212c9de6784641c5e8f0a478
TollanBerhanu <tollanberhanu@gmail.com>
2023-07-10 11:47:53 +0000

    feat: setup global state

--- code/off-chain/src/App.tsx
+++ code/off-chain/src/App.tsx
@@ -1,23 +1,70 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction, createContext, useState, useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Routes,
     Route
 } from 'react-router-dom';
 import Layout from './Layout/Layout';
+import { Address, Blockfrost, Lucid } from 'lucid-cardano';
 
-function App() {
+
+export type AppState = {
+  lucid?: Lucid;
+  walletAddress?: Address;
+  nftTokenNameHex?: string;
+  default?: string
+}
+
+const initialAppState: AppState = {
+  default: 'default'
+};
+
+export const AppStateContext = createContext<{
+  appState: AppState;
+  setAppState: Dispatch<SetStateAction<AppState>>;
+}>({ appState: initialAppState, setAppState: () => {} });
+
+export default function App() {
+
+  const [appState, setAppState] = useState<AppState>({})  // {} is the initial AppState
+
+  const getLucidInstance = async (): Promise<Lucid> => {
+    const lucid = await Lucid.new(
+          new Blockfrost(
+              "https://cardano-preview.blockfrost.io/api/v0",
+              'previewTboSqp1nB894P6wgrGA1PBv8rgUvIS5v'
+          ),
+          "Preview"
+      );
+      if (!window.cardano.nami) {
+          window.alert("Please install Nami Wallet");
+          // return;
+      }
+      const nami = await window.cardano.nami.enable();
+      lucid.selectWallet(nami);
+
+      setAppState({
+        lucid: lucid,
+        walletAddress: await lucid.wallet.address()
+      })
+
+      return lucid;
+  };
+
+  useEffect(() => {
+    if (appState.lucid) return;
+    getLucidInstance();
+  }, [appState]);
+
   return (
     <Router>
       <Routes>
         <Route path="*" element={
-          <div>
+          <AppStateContext.Provider value={ { appState, setAppState } }>
             <Layout />
-          </div>
+          </AppStateContext.Provider>
         }/>
       </Routes>
     </Router>
   );
 }
-
-export default App;
